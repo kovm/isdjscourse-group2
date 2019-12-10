5a31e74f3edf9cd4cc4949c37e8e8c54dd0c548f
Gudvezer <44896237+Gudvezer@users.noreply.github.com>
2019-12-10 16:35:05 +0200

    Update index.js

--- homework/nika/index.js
+++ homework/nika/index.js
@@ -1,3 +1,15 @@
+var a = [4, 10, 125, 65, 2, 16, 27, 47];
 
 document.write(a + "</br>");
 
+for(var i = 0; i < a.length; i++){
+  for(var j = i; j < a.length; j++){
+    if(a[j] < a[i]){
+        var t = a[j];
+        a[j] = a[i];
+        a[i] = t;
+    }
+  }
+}
+
+document.write(a);
