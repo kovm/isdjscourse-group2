8a3286b49c6a830bb0b62fb3cd4359890c56897e
Kateryna <sazonova.k.y@gmail.com>
2019-12-12 14:49:13 +0200

    palindrom

--- homework/kasa/index.js
+++ homework/kasa/index.js
@@ -29,3 +29,16 @@ let guessNumber = (start, end, x) => {
 }
 
 guessNumber(0, 100, 10);
+
+function isPalindrom(str){
+	let arr = str.toLowerCase().split('');
+    let reverseArr = [];
+
+    for (let i = 0; i < arr.length; i++){
+      reverseArr.unshift(arr[i]);
+    }
+
+    return arr.join('') === reverseArr.join('');
+}
+
+console.log(isPalindrom('345543'));
