e067a596a5a9e357d6f723be4b98347376ee4afd
YoungDima <dmitry.misharev@gmail.com>
2019-12-12 15:04:18 +0200

    Homework/2/dmmi (#40)
    * FizzBuzz part
    
    * isPalindrome was added
    
    * safer version of isPalindrome

--- homework/dmmi/index.js
+++ homework/dmmi/index.js
@@ -1,31 +1,38 @@
-let total = 0, count = 1;
-while (count <= 10) {
-  total += count;
-  count += 1;
-}
-console.log(`This is a total ${total/ 12}`);
-console.log(typeof null);
-console.log(1 === 1);
-console.log('10' + '1');
-
-function d(f,s){
-    return f + s(2);
+function fizzBuzz (size) {
+   for (let i = 1; i <= size; i++) {
+      const output = (i % 3 === 0 ? 'Fizz' : '') + (i % 5 === 0 ? 'Buzz' : '');
+      console.log(output === '' ? i : output);
+   }
 }
 
-function s(n){
-    return 2+n;
+fizzBuzz(100);
+
+function isPalindrome (input) {
+   if (typeof input !== 'string' && typeof input !== 'number') {
+      return false;
+   }      
+   const inputArray = input.toString().toLowerCase().split('');
+   for (let i = 0; i <= inputArray.length / 2 - 1; i++) {
+      if (inputArray[i] !== inputArray[inputArray.length - 1 - i]) {
+         return false;
+      }
+   }
+   return true;
 }
 
-function f1() {
-  console.log(typeof this);
-  this.name = "some name 2";
-  return this.name;
-}
+console.log(`sPalindrome for null === ${isPalindrome(null)}`);
+console.log(`isPalindrome for Потоп === ${isPalindrome('Потоп')}`);
+console.log(`isPalindrome for потоп === ${isPalindrome('потоп')}`);
+console.log(`isPalindrome for кот === ${isPalindrome('кот')}`);
+console.log(`isPalindrome for 12314 === ${isPalindrome(12314)}`);
+console.log(`isPalindrome for 345543 === ${isPalindrome(345543)}`);
 
-f1inst = new f1();
+console.log(`isPalindrome for 3 === ${isPalindrome(3)}`);
+console.log(`isPalindrome for 33 === ${isPalindrome(33)}`);
+console.log(`isPalindrome for 353 === ${isPalindrome(353)}`);
+console.log(`isPalindrome for 34 === ${isPalindrome(34)}`);
 
-console.log(d(5,s));
-f1.name = "some name";
-console.log(typeof d);
-console.log(f1());
-console.log(f1inst.name);
+console.log(`isPalindrome for {toString: () => 'kek'} === ${isPalindrome({toString: () => 'kek'})}`);
+console.log(`isPalindrome for () === ${isPalindrome()}`)
+console.log(`isPalindrome for [1,2,3,2,1] === ${isPalindrome([1,2,3,2,1])}`);
+
