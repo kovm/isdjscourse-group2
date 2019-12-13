b3be6d741afd90894027afc679065e83bb1bf48c
rsv-k <rsv.kpast@gmail.com>
2019-12-13 13:57:58 +0200

    second homework

--- homework/sero/index.js
+++ homework/sero/index.js
@@ -1,20 +1,33 @@
-function sum(a) {
-
-    let currentSum = a;
-
-    function f(b) {
-        currentSum += b;
-        return f;
-    }
-
-    f.toString = function() {
-        return currentSum;
-    };
-
-    return f;
+function fizzBuzz () {
+   for (let i = 1; i <= 100; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+         console.log('FizzBuzz');
+      } else if (i % 3 === 0) {
+         console.log('Fizz');
+      } else if (i % 5 === 0) {
+         console.log('Buzz');
+      } else {
+         console.log(i);
+      }
+   }
 }
-  
-consoe.log( sum(1)(2) ); // 3
-consoe.log( sum(5)(-1)(2) ); // 6
-consoe.log( sum(6)(-1)(-2)(-3) ); // 0
-consoe.log( sum(0)(1)(2)(3)(4)(5) ); // 15
+fizzBuzz();
+
+function isPalindrome (str) {
+   str = String(str).toLowerCase();
+
+   let reversed = '';
+
+   for (let i = str.length - 1; i >= 0; i--) {
+      reversed += str[i];
+   }
+
+   return str === reversed;
+}
+
+console.log(isPalindrome('civic')); // true
+console.log(isPalindrome('deified')); // true
+console.log(isPalindrome('Hannah')); // true
+console.log(isPalindrome('definetely not a palindrome')); // false
+console.log(isPalindrome(12314)); // false
+console.log(isPalindrome(345543)); // true
