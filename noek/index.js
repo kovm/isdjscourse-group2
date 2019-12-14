fa50f88a6166635891fcc161d43e36747816d261
KateNosul <hinatanya14@gmail.com>
2019-12-14 20:10:21 +0200

    second git

---
+++ noek/index.js
@@ -0,0 +1,22 @@
+function code(){
+let i = 1;
+let N = 100;
+
+for (i; i<=N; i++){
+
+    if (i % 3 === 0 && i % 5 === 0){
+
+        console.log('FizzBuzz');
+
+    } else if (i % 5 === 0){
+
+        console.log('Buzz');
+
+    }else if(i % 3 === 0){
+
+        console.log('Fizz');
+
+    } else console.log(i);
+}
+return;
+}
