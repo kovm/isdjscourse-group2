87bd3a5d17b54049aee59fa1993b674c794966ca
egor-nikulicov <nikulicov@gmail.com>
2019-12-15 00:42:25 +0200

    Second homework is done

--- homework/yeni/index.js
+++ homework/yeni/index.js
@@ -1,3 +1,44 @@
+/* Second homework */
+
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
+        console.log("Fizz");
+        continue;
+    } 
+    if (i % 5 == 0) {
+        console.log("Buzz");
+        continue;
+    }
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+        continue;
+    }
+    console.log(i);
+}
+
+//*
+function CheckPolyndrom(val) {
+    var boolFlag = false;
+    for (var i = 0; i < val.length; i++) {
+        if (val[i] != val[val.length - i - 1]) {
+            boolFlag = true;
+            break;
+        }
+    }
+    return boolFlag ? "This is not polyndrom" : "This is polyndrom";
+}
+
+var masPol = [1,2,3,3,2,1];
+var masNoPol = [1,5,7,2,1];
+var stringPol = "asdfghhgfdsa";
+var stringNoPol = "qazxswedcvfrtgbnhy";
+
+console.log(CheckPolyndrom(masPol));
+console.log(CheckPolyndrom(masNoPol));
+console.log(CheckPolyndrom(stringPol));
+console.log(CheckPolyndrom(stringNoPol));
+
+/* First homework
 var masDiv = [];
 
 function NewDiv(number, Object) {
@@ -24,4 +65,4 @@ function DeleteDiv() {
         }
         masDiv = [];
     }
-}
+}*/
