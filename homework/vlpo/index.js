8e6b981b7b758463349a89f86ec50934d165e49c
Vladislav Podgrebelnyi <frostigsinn@gmail.com>
2019-12-15 19:35:25 +0200

    Fixed code style

--- homework/vlpo/index.js
+++ homework/vlpo/index.js
@@ -1,28 +1,27 @@
-
-function multiplicity() {
-  let number = 1;
-  while (number <= 100) {
-    if ((number % 5 === 0) && (number % 3 === 0)) {
-      console.log('FizzBuzz');
-    } else if (number % 3 === 0) {
-      console.log('Fizz');
-    } else if (number % 5 === 0) {
-      console.log('Buzz');
-    } else {
-      console.log(number);
-    }
-    number += 1;
-  }
+function multiplicity () {
+   let number = 1;
+   while (number <= 100) {
+      if ((number % 5 === 0) && (number % 3 === 0)) {
+         console.log('FizzBuzz');
+      } else if (number % 3 === 0) {
+         console.log('Fizz');
+      } else if (number % 5 === 0) {
+         console.log('Buzz');
+      } else {
+         console.log(number);
+      }
+      number += 1;
+   }
 }
 
-function isPalindrom(someText) {
-  const tempStr = someText.toString().toLowerCase();
-  for (let i = 0; i < tempStr.length / 2; i++) {
-    if (tempStr[i] !== tempStr[tempStr.length - i - 1]) {
-      return false;
-    }
-  }
-  return true;
+function isPalindrom (someText) {
+   const tempStr = someText.toString().toLowerCase();
+   for (let i = 0; i < tempStr.length / 2; i++) {
+      if (tempStr[i] !== tempStr[tempStr.length - i - 1]) {
+         return false;
+      }
+   }
+   return true;
 }
 
 multiplicity();
