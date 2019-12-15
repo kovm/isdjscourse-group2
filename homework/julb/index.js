7ac747c16cc943c2048265df2dd71074656eafe2
Yulya Lebed <yulya@designingit.com>
2019-12-15 22:58:26 +0200

    homework N2

---
+++ homework/julb/index.js
@@ -0,0 +1,35 @@
+function checkNumber () {
+   let num = 1;
+
+   while (num <= 100) {
+      if (num % 3 === 0 && num % 5 === 0) {
+         console.log('FizzBuzz');
+      } else if (num % 3 === 0) {
+         console.log('Fizz');
+      } else if (num % 5 === 0) {
+         console.log('Buzz');
+      } else {
+         console.log(num);
+      }
+
+      num++;
+   }
+}
+
+checkNumber();
+
+function isPalindrom (str) {
+   str = str.toString().toLowerCase();
+
+   for (let i = 0; i <= str.length - 1; i++) {
+      if (str[i] !== str[str.length - 1 - i]) {
+         return console.log('не палиндромом');
+      }
+   }
+   return console.log('палиндромом');
+}
+
+isPalindrom('лопата');
+isPalindrom('потоп');
+isPalindrom('23455432');
+isPalindrom('12354321');
