484880d42eac5f7fcd1d3261e40669ca3bec2ea8
Vladislav Podgrebelnyi <frostigsinn@gmail.com>
2019-12-15 18:49:45 +0200

    Homework 2

--- homework/vlpo/index.js
+++ homework/vlpo/index.js
@@ -1,18 +1,32 @@
-var numbers = [3, -6, 9, 10, 0, 8, 2, -18, 15];
-console.log(numbers);
 
-selectionSort(numbers);
-
-console.log(numbers);
-
-function selectionSort(someArray) {
-    for(var i = 0; i < numbers.length; i++) {
-        minIndex = i;
-        for(var j = i + 1; j < numbers.length; j++) {
-            if(numbers[minIndex] > numbers[j])
-                minIndex = j;
-        }
-        if(minIndex !== i)
-            [numbers[minIndex], numbers[i]] = [numbers[i], numbers[minIndex]];
+function multiplicity() {
+  let number = 1;
+  while (number <= 100) {
+    if ((number % 5 === 0) && (number % 3 === 0)) {
+      console.log('FizzBuzz');
+    } else if (number % 3 === 0) {
+      console.log('Fizz');
+    } else if (number % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(number);
     }
+    number += 1;
+  }
 }
+
+function isPalindrom(someText) {
+  const tempStr = someText.toString().toLowerCase();
+  for (let i = 0; i < tempStr.length / 2; i++) {
+    if (tempStr[i] !== tempStr[tempStr.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+multiplicity();
+console.log(isPalindrom('Racecar'));
+console.log(isPalindrom('Car'));
+console.log(isPalindrom(234432));
+console.log(isPalindrom(23456));
