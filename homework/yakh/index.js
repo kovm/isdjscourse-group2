b83cc1cec109d2cbb8ef2792080d55bace151935
bizzy1227 <narkoman1227@gmail.com>
2019-12-16 11:07:43 +0200

    verified eslint

--- homework/yakh/index.js
+++ homework/yakh/index.js
@@ -1,38 +1,38 @@
-function fizzBuzz(data){
-    for(let i = 1; i <= data; i++){
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log('FizzBuzz');
-            continue;
-        }
-        if(i % 3 === 0){
-            console.log('Fizz');
-            continue;
-        }
-        if (i % 5 === 0){
-            console.log('Buzz');
-            continue;
-        }  
-    console.log(i);
-    }
+function fizzBuzz (data) {
+   for (let i = 1; i <= data; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+         console.log('FizzBuzz');
+         continue;
+      }
+      if (i % 3 === 0) {
+         console.log('Fizz');
+         continue;
+      }
+      if (i % 5 === 0) {
+         console.log('Buzz');
+         continue;
+      }
+      console.log(i);
+   }
 }
 
 fizzBuzz(100);
 
-function isPalindrom(data){
-    let revers  = [];
-    arr = String(data).toLowerCase().split('');
-    for(let i = arr.length-1; i >= 0; i--){
-        revers.push(arr[i]);
-    }
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] !== revers[i]){
-            return false;
-        }
-    }
-    return true;
+function isPalindrom (data) {
+   const revers = [];
+   const arr = String(data).toLowerCase().split('');
+   for (let i = arr.length - 1; i >= 0; i--) {
+      revers.push(arr[i]);
+   }
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== revers[i]) {
+         return false;
+      }
+   }
+   return true;
 }
 
-console.log(isPalindrom(123123123));
-console.log(isPalindrom(12122121));
-console.log(isPalindrom('ПоТоП'));
-console.log(isPalindrom('нож'));
+console.log('\nisPalindrom 123123123: ' + isPalindrom(123123123));
+console.log('\nisPalindrom 12122121: ' + isPalindrom(12122121));
+console.log('\nisPalindrom ПоТоП: ' + isPalindrom('ПоТоП'));
+console.log('\nisPalindrom нож: ' + isPalindrom('нож'));
