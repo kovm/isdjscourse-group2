0f25c5cef99cfc87ac2a77d955566511e18cb5c8
Fox2510 <admin@3zweb.com.ua>
2019-12-17 18:13:38 +0200

    зд2

--- homework/depo/index.js
+++ homework/depo/index.js
@@ -1,1 +1,13 @@
-console.log(`Hello`);
+let a = 1;
+while (a <= 100) {
+  if (!(a % 3) && !(a % 5)) {
+    console.log('FizzBuzz')
+  } else if (!(a % 5 )) {
+    console.log('Buzz')
+  } else if (!(a % 3)) {
+    console.log('Fizz')
+  } else {
+    console.log(a)
+  }
+  a++
+}
