724f69118a5b1921ba8c9ad57fd51f3e6b43d744
AnaMore21 <anastasiakrut1@gmail.com>
2019-12-18 00:59:33 +0200

    Home work 2 (FizzBuzz, isPalindrome)

--- homework/anhl/index.js
+++ homework/anhl/index.js
@@ -1,41 +1,30 @@
-//Игра - камень ножницы бумага
-var compStep = Math.floor(Math.random()*10);
-console.log("Цифра компа-" + compStep);
-
-if (compStep < 3.33) {
-  compStep = "камень";
-} else if (compStep >= 3.33 && compStep <= 6.66 ) {
-  compStep = "ножницы";
-} else if (compStep > 6.66) {
-  compStep = "бумага";
+function num () {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz')
+    } else if (i % 3 === 0) {
+      console.log('Fizz')
+    } else if (i % 5 === 0) {
+      console.log('Buzz')
+    } else {
+      console.log(i)
+    }
+  }
 }
-console.log("Комп выбрал-" +compStep);
+num()
 
+let word = prompt('Give me your word')
+word = word.toLowerCase()
+let result = ''
 
-var playerStep = prompt('Choose камень, ножницы или бумага :)');
-console.log("Ввёл игрок-" +playerStep);
-
-if (playerStep === null) {
-  alert ("Choose AGAIN");
-} else {
-  playerStep = playerStep.toLowerCase();
+function isPalindrome (word) {
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] === word[word.length - 1 - i]) {
+      result = 'true'
+    } else {
+      result = 'false'
+    }
+  }
+  console.log(result)
 }
-console.log("Измена на мал.регистр-" +playerStep);
-alert("Комп выбрал-" +compStep);
-
-//4
-if (compStep === playerStep) {
-  alert ("Вы и комп выбрали "+compStep+" - НИЧЬЯ :)");
-} else if (compStep === "камень" && playerStep === "ножницы") {
-  alert("ПРОИГРАЛИ! Комп выбрал-камень, Вы-ножницы");
-} else if (compStep === "камень" && playerStep === "бумага") {
-  alert("УРА! WIN! Комп выбрал-камень, Вы-бумага");
-} else if (compStep === "ножницы" && playerStep === "бумага") {
-  alert("ПРОИГРАЛИ! Комп выбрал-ножницы, Вы-бумага");
-} else if (compStep === "ножницы" && playerStep === "камень") {
-  alert("УРА! WIN! Комп выбрал-ножницы, Вы-камень");
-} else if (compStep === "бумага" && playerStep === "камень") {
-  alert("ПРОИГРАЛИ! Комп выбрал-бумага, Вы-камень");
-} else if (compStep === "бумага" && playerStep === "ножницы") {
-  alert("УРА! WIN! Комп выбрал-бумага, Вы-ножницы");
-}
+isPalindrome(word)
