6e18b3dce61f6ebfc35b0dd23dc765a7c7f1b334
tadm <goodmasterd@gmail.com>
2019-12-17 19:23:40 +0200

    homework2

--- homework/tadm/index.js
+++ homework/tadm/index.js
@@ -1,45 +1,45 @@
-let split = function(number,number2) {
+const FizzBuzz = function (number, number2) {
    for (let i = 1; i < 101; i++) {
-   
-     if ((i % number === 0) && (i % number2 === 0)){
-       console.log ('FizzBuzz');
-       continue
-     }
-     if (i % number === 0) {
-       console.log ('Fizz');
-       continue
+      if ((i % number === 0) && (i % number2 === 0)) {
+         console.log('FizzBuzz');
+         continue;
+      }
+      if (i % number === 0) {
+         console.log('Fizz');
+         continue;
+      }
+      if (i % number2 === 0) {
+         console.log('Buzz');
+         continue;
+      } else {
+         console.log(i);
+      }
    }
-     if  (i % number2 === 0) {
-           console.log ('Buzz'); 
-           continue  
-   } 
-     else {  
-             console.log (i);
-       }   
-   }
-   };
-   
-   // split(3,5);
+};
 
-   let  isPalindrom = function(any) {
-      let oldThing = any;
-      let newThing = '';
-      let num = 1;
-      
-      if (typeof oldThing === typeof num){
-        oldThing = oldThing.toString();  
-      }
-      
-      for (let i = 1; i <= oldThing.length; i++) {
-        newThing += oldThing.charAt(oldThing.length -i)
-      }
-      
-      if (oldThing === newThing){
-        console.log(`${oldThing} является палиндромом`);
-      }
-      else {
-        console.log(`${oldThing} не является палиндромом`);
-      }
-      };
-      
-      // isPalindrom(7887);
+FizzBuzz(3, 5);
+
+const isPalindrom = function (any) {
+   if (typeof any !== 'string' && typeof any !== 'number') {
+      console.log(`${any} - is an exception`);
+      return false;
+   }
+   const oldThing = any.toString().toLowerCase();
+   let newThing = '';
+   for (let i = 1; i <= oldThing.length; i++) {
+      newThing += oldThing.charAt(oldThing.length - i);
+   }
+   if (oldThing === newThing) {
+      console.log(`${any} - is palindrom`);
+   } else {
+      console.log(`${any} - is not palindrom`);
+   }
+};
+
+isPalindrom('Потоп');
+isPalindrom(345543);
+isPalindrom('Word palindrom');
+isPalindrom();
+isPalindrom(null);
+isPalindrom([1, 2, 1]);
+isPalindrom({});
