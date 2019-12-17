c1b0d33a77667d03ef24973068677095add0471d
tadm <goodmasterd@gmail.com>
2019-12-17 12:23:59 +0200

    homework2

--- homework/tadm/index.js
+++ homework/tadm/index.js
@@ -1,25 +1,45 @@
-let numbArray = [46.67, 31.89, 16.02, 88.51];
+let split = function(number,number2) {
+   for (let i = 1; i < 101; i++) {
+   
+     if ((i % number === 0) && (i % number2 === 0)){
+       console.log ('FizzBuzz');
+       continue
+     }
+     if (i % number === 0) {
+       console.log ('Fizz');
+       continue
+   }
+     if  (i % number2 === 0) {
+           console.log ('Buzz'); 
+           continue  
+   } 
+     else {  
+             console.log (i);
+       }   
+   }
+   };
+   
+   // split(3,5);
 
-changeArray (numbArray,"round");
-
-function changeArray (anyArray,method){
-
-    for (let i = 0; i < anyArray.length; i++) {   
-
-   if (method == "round") {
-   anyArray[i] = Math.round (anyArray[i]);
-   console.log (anyArray[i]); 
-}
-else if (method == "ceil") {
-   anyArray[i] = Math.ceil (anyArray[i]);
-   console.log (anyArray[i]);
-}
-else if (method == "floor") {
-   anyArray[i] = Math.floor (anyArray[i]);
-   console.log (anyArray[i]);
-}
-else {
-    console.log ('Вы ввели не правельный метод ');
-}
-}
-}
+   let  isPalindrom = function(any) {
+      let oldThing = any;
+      let newThing = '';
+      let num = 1;
+      
+      if (typeof oldThing === typeof num){
+        oldThing = oldThing.toString();  
+      }
+      
+      for (let i = 1; i <= oldThing.length; i++) {
+        newThing += oldThing.charAt(oldThing.length -i)
+      }
+      
+      if (oldThing === newThing){
+        console.log(`${oldThing} является палиндромом`);
+      }
+      else {
+        console.log(`${oldThing} не является палиндромом`);
+      }
+      };
+      
+      // isPalindrom(7887);
