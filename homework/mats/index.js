2b9ffdb05aa564f4bf0368526be28ab2536efed7
Maxim Tsalyuk <max.tsalyuk@gmail.com>
2019-12-17 21:26:43 +0200

    eslint corrected, isPadindrom toLowerCase() added

--- homework/mats/index.js
+++ homework/mats/index.js
@@ -1,39 +1,35 @@
-function fizzBuzz() {
-  for (let i = 1; i <= 100; i++)
-  {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
-    } else if (i % 3 === 0) {
-      console.log("Fizz");
-    } else if (i % 5 === 0) {
-      console.log("Buzz");
-    } else {
-      console.log(i);
-    }
-  }
+function fizzBuzz () {
+   for (let i = 1; i <= 100; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+         console.log('FizzBuzz');
+      } else if (i % 3 === 0) {
+         console.log('Fizz');
+      } else if (i % 5 === 0) {
+         console.log('Buzz');
+      } else {
+         console.log(i);
+      }
+   }
 }
 
+function isPalindrom (value) {
+   const stringValue = value.toString().toLowerCase();
+   let valueReversed = '';
 
-function isPalindrom(value) {
-  let stringValue = value.toString();
-  let valueReversed = "";
+   for (let i = stringValue.length - 1; i >= 0; i--) {
+      valueReversed += stringValue[i];
+   }
+   console.log('StringValue: ' + stringValue + '\nReversed: ' + valueReversed);
 
-
-  for (let i = stringValue.length - 1; i >= 0; i--)
-  {
-    valueReversed += stringValue[i];
-  }
-
-
-  if (stringValue === valueReversed) {
-    return true;
-  } else {
-    return false;
-  }
+   if (stringValue === valueReversed) {
+      return true;
+   } else {
+      return false;
+   }
 }
-
 
 // Main
 fizzBuzz();
-console.log(isPalindrom("kek"));
+console.log(isPalindrom('kek'));
 console.log(isPalindrom(123321));
+console.log(isPalindrom('Kek'));
