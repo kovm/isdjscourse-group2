5f916125a8abab68037d46f6cce1587c76bdb741
Stanislava <lesnaya2018@gmail.com>
2019-12-17 14:36:23 +0200

    error correction

--- homework/rsni/index.js
+++ homework/rsni/index.js
@@ -1,27 +1,27 @@
 function num () {
-  for (let i = 1; i <= 100; i ++) {
-    if ( i % 3 === 0 && i % 5 === 0) {
-      console.log ('FizzBuzz');
-    } else if (i % 3 === 0) {
-        console.log ('Fizz');
+   for (let i = 1; i <= 100; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+         console.log('FizzBuzz');
+      } else if (i % 3 === 0) {
+         console.log('Fizz');
       } else if (i % 5 === 0) {
-          console.log ('Buzz');
-        } else {
-          console.log(i)
-        }
+         console.log('Buzz');
+      } else {
+         console.log(i);
       }
-    }
-num ();
+   }
+}
+num();
 
 function isPalindrom (val) {
-  val = String(val);
-  val = val.toLowerCase();
-  for (let i = 0; i < val.length / 2; i++) {
-    if (val[i] !== val[val.length -1 -i]) {
-      return false;
-    }
-  }
-  return true;
+   val = String(val);
+   val = val.toLowerCase();
+   for (let i = 0; i < val.length / 2; i++) {
+      if (val[i] !== val[val.length - 1 - i]) {
+         return false;
+      }
+   }
+   return true;
 }
 console.log(isPalindrom(2442));
 console.log(isPalindrom(2446));
