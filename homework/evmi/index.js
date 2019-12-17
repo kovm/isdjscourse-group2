7b3d0b9f719510398bf4a9cb5e83681d6861f0b7
EvaHet <evgeniya.mishchenko93@gmail.com>
2019-12-17 18:22:56 +0200

    Homework №2

--- homework/evmi/index.js
+++ homework/evmi/index.js
@@ -1,25 +1,31 @@
-function Library() {
-    this.bookList = [];
-};
+// 1
+const palindrom = prompt('Input a word:')
 
+function isPalindrom (palindrom) {
+  palindrom = palindrom.toLowerCase()
+  const len = palindrom.length - 1
+  for (let i = 0; i < len / 2; i++) {
+    if (palindrom[i] !== palindrom[len - i]) {
+      return false
+    }
+  }
+  return true
+}
+console.log(isPalindrom(palindrom))
 
-Library.prototype.addBook = function (title, author, year) {
-    let boo = new Book(title, author, year);
-    this.bookList.push(boo);
-    return boo;
-};
-Library.prototype.getBooks = function () {
-    return library.bookList;
-};
+// 2
+function num () {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+      document.write('FizzBuzz', '<br />')
+    } else if (i % 3 === 0) {
+      document.write('Fizz', '<br />')
+    } else if (i % 5 === 0) {
+      document.write('Buzz', '<br />')
+    } else {
+      document.write(i + '<br />', ' ')
+    }
+  }
+}
 
-
-function Book(title, author, year) {
-    this.title = title;
-    this.author = author;
-    this.year = year;
-};
-
-let library = new Library();
-library.addBook("Eloquent JavaScript 3rd Edition", "Marijn Haverbeke", 2018);
-library.addBook("JavaScript", "Vasylyna", 1625);
-console.log(library.getBooks());
+num()
