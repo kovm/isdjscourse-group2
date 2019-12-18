6c83cef08a244c47e7c7b132e0346ff36b113a1d
Gudvezer <44896237+Gudvezer@users.noreply.github.com>
2019-12-18 12:46:18 +0200

    Update index.js

--- homework/nika/index.js
+++ homework/nika/index.js
@@ -1,3 +1,29 @@
-
-document.write(a + "</br>");
-
+function oneToHundred () {
+   for (let i = 1; i < 101; i++) {
+      if ((i % 3 === 0) && !(i % 5 === 0)) {
+         console.log('Fizz');
+      } else if ((i % 5 === 0) && !(i % 3 === 0)) {
+         console.log('Buzz');
+      } else if ((i % 5 === 0) && (i % 3 === 0)) {
+         console.log('FizzBuzz');
+      } else {
+         console.log(i);
+      }
+   }
+}
+function isPalindrome (str) {
+   if (typeof str === 'number') {
+      str = String(str);
+   }
+   const strLen = str.length;
+   let result = '';
+   for (let i = 0; i < strLen; i++) {
+      if (str[i] === str[strLen - 1 - i]) {
+         result = 'true';
+      } else {
+         result = 'false';
+         return result;
+      }
+   }
+   return result;
+}
