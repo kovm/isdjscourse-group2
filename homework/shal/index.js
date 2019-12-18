7fdff299c37161e6dcde18c1a67df52e4232de88
Alexandr Shlyakhov <alex.06.sandr@gmail.com>
2019-12-18 12:58:10 +0200

    new homework 2

--- homework/shal/index.js
+++ homework/shal/index.js
@@ -1,15 +1,30 @@
-function shipping() {
-console.log('checkout/shipping_method');
- $.ajax({
-        url: 'index.php?route=checkout/shipping_method',
-        dataType: 'html',
-        success: function(html) {
-            $('.ship-pay').html(html);	
-			
-			payment();			
-        },
-        error: function(xhr, ajaxOptions, thrownError) {
-            alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
+function fizz(num) {
+    for (let i = 0; i <= num; i++) {
+        if (i % 3 === 0) {
+            console.log(i + 'Fizz' + '\n');
+        } else if (i % 5 === 0) {
+            console.log(i + 'Buzz' + '\n');
         }
-    });	
+        if (i % 5 === 0 && i % 3 === 0) {
+            console.log(i + 'FizzBuzz' + '\n');
+        }
+    }
 }
+
+function pali(text) {
+    let arr = text.toString().toLowerCase().split('');
+    let res;
+    for (var i = 0; i < arr.length / 2 - 1; i++) {
+        if (arr[i] !== arr[arr.length - 1 - i]) {
+            res = 'no';
+        } else {
+            res = 'yes';
+        }
+    }
+    console.log(res);
+}
+fizz(100);
+pali('потовп');
+pali('Потоп');
+pali('123321');
+pali('1233212');
