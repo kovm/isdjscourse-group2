633a5f0c0c91e2adef02b3cad0538d51953f99f6
AnaMore21 <anastasiakrut1@gmail.com>
2019-12-30 23:11:21 +0200

    Upgraded to the latest version master and checked eslint, it seems

--- homework/anhl/index.js
+++ homework/anhl/index.js
@@ -1,30 +1,30 @@
 function num () {
-  for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log('FizzBuzz')
-    } else if (i % 3 === 0) {
-      console.log('Fizz')
-    } else if (i % 5 === 0) {
-      console.log('Buzz')
-    } else {
-      console.log(i)
-    }
-  }
+   for (let i = 1; i <= 100; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+         console.log('FizzBuzz');
+      } else if (i % 3 === 0) {
+         console.log('Fizz');
+      } else if (i % 5 === 0) {
+         console.log('Buzz');
+      } else {
+         console.log(i);
+      }
+   }
 }
-num()
+num();
 
-let word = prompt('Give me your word')
-word = word.toLowerCase()
-let result = ''
+let word = prompt('Give me your word');
+word = word.toLowerCase();
+let result = '';
 
 function isPalindrome (word) {
-  for (var i = 0; i < word.length; i++) {
-    if (word[i] === word[word.length - 1 - i]) {
-      result = 'true'
-    } else {
-      result = 'false'
-    }
-  }
-  console.log(result)
+   for (var i = 0; i < word.length; i++) {
+      if (word[i] === word[word.length - 1 - i]) {
+         result = 'true';
+      } else {
+         result = 'false';
+      }
+   }
+   console.log(result);
 }
-isPalindrome(word)
+isPalindrome(word);
