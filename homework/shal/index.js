816386f26b9d662a399c491a60ecde823b82bec2
Alexandr Shlyakhov <alex.06.sandr@gmail.com>
2020-01-03 23:53:08 +0200

    edit with eslint

--- homework/shal/index.js
+++ homework/shal/index.js
@@ -1,30 +1,30 @@
 function fizz(num) {
     for (let i = 0; i <= num; i++) {
         if (i % 3 === 0) {
-            console.log(i + 'Fizz' + '\n');
+            console.log(i + "Fizz" + "\n");
         } else if (i % 5 === 0) {
-            console.log(i + 'Buzz' + '\n');
+            console.log(i + "Buzz" + "\n");
         }
         if (i % 5 === 0 && i % 3 === 0) {
-            console.log(i + 'FizzBuzz' + '\n');
+            console.log(i + "FizzBuzz" + "\n");
         }
     }
 }
 
 function pali(text) {
-    let arr = text.toString().toLowerCase().split('');
+    let arr = text.toString().toLowerCase().split("");
     let res;
     for (var i = 0; i < arr.length / 2 - 1; i++) {
         if (arr[i] !== arr[arr.length - 1 - i]) {
-            res = 'no';
+            res = "no";
         } else {
-            res = 'yes';
+            res = "yes";
         }
     }
     console.log(res);
 }
 fizz(100);
-pali('потовп');
-pali('Потоп');
-pali('123321');
-pali('1233212');
+pali("потовп");
+pali("Потоп");
+pali("123123");
+pali("1233212");
