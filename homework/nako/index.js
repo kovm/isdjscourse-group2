52789239a8cb82b7cbac874f106cca0982b76a05
Козак Наталья Ивановна <nkozak@netomega.corp.local>
2020-01-09 17:17:07 +0200

    index.js

--- homework/nako/index.js
+++ homework/nako/index.js
@@ -1,30 +1,78 @@
-function fizzBuzz () {
-   for (let i = 1; i <= 100; i++) {
-      if ((i % 3 === 0 && i % 5 === 0)) {
-         console.log('FizzBuzz');
-      } else if (i % 5 === 0) {
-         console.log('Buzz');
-      } else if (i % 3 === 0) {
-         console.log('Fizz');
-      } else {
-         console.log(i);
+function createArea () {
+   let arr = [10];
+   for (let i = 0; i < 10; i++) {
+      arr[i] = [10];
+      for (let j = 0; j < 10; j++) {
+         arr[i][j] = 0;
       }
    }
-};
+   return arr;
+}
 
-fizzBuzz();
+function setShips (area) {
+   let ships = [{
+      length: 4,
+      count: 1
+   }, {
+      length: 3,
+      count: 2
+   }, {
+      length: 2,
+      count: 3
+   }, {
+      length: 1,
+      count: 4
+   }];
 
-function isPalindrome (str) {
-   if ((typeof str !== 'string') && (typeof str !== 'number')) {
-      return false;
-   }
-   str += '';
-   for (let i = 0; i < str.length; i++) {
-      if (str[i].toLowerCase() !== str[str.length - (i + 1)].toLowerCase()) {
-         return false;
+   let n = 0;
+   let set = false;
+   for (let ship of ships) {
+      set = false;
+
+      for (let i = ship.count; i > 0; i--) {
+
+         do {
+            let kx = Math.round(Math.random() * 1), ky = +!kx;
+            let x, y;
+            let coordinates = [];
+            if (kx) {x = Math.round(Math.random() * (10 - ship.length)), y = Math.floor(Math.random() * 10);}
+            else {x = Math.round(Math.random() * 10), y = Math.floor(Math.random() * (10-ship.length));}
+            for (let j = 0; j < ship.length; j++) {
+
+               if (area[x+=kx][y+=ky] !== 0 ) break;
+               coordinates.push[x+=kx][y+=ky];
+            }
+            if (coordinates.length===0) continue;
+            if (kx) {
+               if ()
+            }
+            else {
+
+            }
+
+
+
+            }
+         while (!set);
+
       }
-   }
-   return true;
-};
 
-isPalindrome('tenet');
+   }
+   console.log(area);
+   return area;
+}
+
+function createSeaBattle () {
+   let area = createArea();
+   area = setShips(area);
+   return function (x) {
+      return function (y) {
+         console.log(x, y);
+
+      };
+   };
+}
+
+let seaBattle = createSeaBattle();
+
+seaBattle(3)(5);
