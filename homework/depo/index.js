62f850102df09dc1a061073cf41b00c7f40e45ce
Fox2510 <admin@3zweb.com.ua>
2020-01-15 11:52:10 +0200

    ship-dz3

--- homework/depo/index.js
+++ homework/depo/index.js
@@ -1,13 +1,52 @@
-let a = 1;
-while (a <= 100) {
-  if (!(a % 3) && !(a % 5)) {
-    console.log('FizzBuzz')
-  } else if (!(a % 5 )) {
-    console.log('Buzz')
-  } else if (!(a % 3)) {
-    console.log('Fizz')
-  } else {
-    console.log(a)
-  }
-  a++
+let b1 = 3; // координаты корабля
+let b2 = 4; // координаты корабля
+
+let b3 = 6; // координаты корабля
+let b4 = 9; // координаты корабля
+
+let b1shot = false;
+let b2shot = false;
+let titanikKilled = false;
+
+let shot; // выстрел
+let pool = 0; // выстрелов
+let hits = 0; // попадания
+
+let ship = false;
+
+while(!ship){
+    shot = prompt("Ты готов мочкануть корабль? (введи от 0-9):");
+if (shot < 0 || shot > 9){
+       alert("Я же просил от 0 до 9 ввести!");
+    }
+    
+
+else{
+    pool++;
+if (shot == b1){
+  b1shot = true;
+  hits = hits + 1;
+  alert("Ранил");
 }
+
+if (shot == b2){
+  b2shot = true;
+  hits = hits + 1;
+  alert("Ранил");
+}  
+
+if(b1shot && b2shot && !titanikKilled){
+  alert("Потопил Титаник");
+  titanikKilled = true;
+}
+
+if(shot == b3 || shot == b4){
+  hits = hits + 1;
+  alert("Убил");
+}
+
+if(hits === 4){
+  ship = true;
+  alert("Победа");
+}
+}}
