30339075e8a11e9ac4b53e71b015bb2a6d07d1fc
Nataly2315 <nataliakozak2017@gmail.com>
2020-01-26 22:27:13 +0200

    part 2

--- homework/nako/index.js
+++ homework/nako/index.js
@@ -1,4 +1,6 @@
-function createArea () {
+
+
+function createArea() {
    let arr = [10];
    for (let i = 0; i < 10; i++) {
       arr[i] = [10];
@@ -9,13 +11,50 @@ function createArea () {
    return arr;
 }
 
-function setShips (area) {
+
+function checkCoordinates(x, y, kx, ky, length, area) {
+   let startX, endX, startY, endY;
+   startX = (x === 0) ? x : x - 1;
+   // если условие истинно - это значит, что корабль расположен вертикально и его последняя палуба примыкает
+   // к нижней границе игрового поля
+   // поэтому координата 'x' последней палубы будет индексом конца цикла
+   if (x + kx * length === 9 && kx === 1) endX = x + kx * length;
+   // корабль расположен вертикально и между ним и нижней границей игрового поля есть, как минимум, ещё
+   // одна строка, координата этой строки и будет индексом конца цикла
+   else if (x + kx * length < 9 && kx === 1) endX = x + kx * length + 1;
+   // корабль расположен горизонтально вдоль нижней границы игрового поля
+   else if (x === 9 && kx === 0) endX = x + 1;
+   // корабль расположен горизонтально где-то по середине игрового поля
+   else if (x < 9 && kx === 0) endX = x + 2;
+
+   // формируем индексы начала и конца цикла для столбцов
+   // принцип такой же, как и для строк
+   startY = (y === 0) ? y : y - 1;
+   if (y + ky * length === 9 && ky === 1) endY = y + ky * length;
+   else if (y + ky * length < 10 && ky === 1) endY = y + ky * length + 1;
+   else if (y === 9 && ky === 0) endY = y + 1;
+   else if (y < 9 && ky === 0) endY = y + 2;
+   // запускаем циклы и проверяем выбранный диапазон ячеек
+   // если значение текущей ячейки равно 1 (там есть палуба корабля)
+   // возвращаем false
+   for (let i = startX; i < endX; i++) {
+      for (let j = startY; j < endY; j++) {
+         if (area[i][j] !== 0) return false;
+      }
+   }
+   return true;
+}
+
+
+
+function createSeaBattle() {
+   let area = createArea();
    let ships = [{
       length: 4,
       count: 1
    }, {
       length: 3,
-      count: 2
+      count: 2,
    }, {
       length: 2,
       count: 3
@@ -23,50 +62,65 @@ function setShips (area) {
       length: 1,
       count: 4
    }];
+   let setShips = function () {
 
-   let n = 0;
-   let set = false;
-   for (let ship of ships) {
-      set = false;
+      let n = 0;
 
-      for (let i = ship.count; i > 0; i--) {
+     // выбираем тип корабля
+      for (let ship of ships) {
+         //количество кораблей данного типа
 
-         do {
-            let kx = Math.round(Math.random() * 1), ky = +!kx;
-            let x, y;
-            let coordinates = [];
-            if (kx) {x = Math.round(Math.random() * (10 - ship.length)), y = Math.floor(Math.random() * 10);}
-            else {x = Math.round(Math.random() * 10), y = Math.floor(Math.random() * (10-ship.length));}
-            for (let j = 0; j < ship.length; j++) {
+         for (let i = ship.count; i > 0; i--) {
+            //маркер удачного располоения корабля
+            let set = false;
 
-               if (area[x+=kx][y+=ky] !== 0 ) break;
-               coordinates.push[x+=kx][y+=ky];
+            do {
+               let kx = Math.round(Math.random()), ky = +!kx;
+               let x, y;
+
+               if (kx) {
+                  x = Math.round(Math.random() * (10 - ship.length));
+                  y = Math.floor(Math.random() * 9);
+               } else {
+                  x = Math.round(Math.random() * 9);
+                  y = Math.floor(Math.random() * (10 - ship.length));
+               }
+
+               let length = ship.length;
+
+               set = checkCoordinates(x, y, kx, ky, length, area);
+
+               if (set) {
+                  for (let i = 1; i <= ship.length; i++) {
+                     area[x][y] = ship.length;
+                     x += kx;
+                     y += ky;
+                  }
+               }
+
+
             }
-            if (coordinates.length===0) continue;
-            if (kx) {
-               if ()
-            }
-            else {
+            while (!set);
 
-            }
-
-
-
-            }
-         while (!set);
+         }
 
       }
 
-   }
-   console.log(area);
-   return area;
-}
+      return area;
+   };
+   let aliveShips = 10;
+   setShips();
 
-function createSeaBattle () {
-   let area = createArea();
-   area = setShips(area);
+
    return function (x) {
       return function (y) {
+         if (typeof x !=="number" || typeof  y !=="number") throw Error("Wrong type of coordinates");
+         if (x>9 || x<0 || y>9 || y<0) throw Error("Wrong range of coordinates");
+         if (!aliveShips) throw Error ("There are no more ships");
+
+         if (area[x][y] === 0) {return -1;}
+
+         else
          console.log(x, y);
 
       };
@@ -75,4 +129,4 @@ function createSeaBattle () {
 
 let seaBattle = createSeaBattle();
 
-seaBattle(3)(5);
+seaBattle(10)(-5);
