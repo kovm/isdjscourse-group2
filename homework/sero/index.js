dd9664bfd8c5e169661764f66a4ed68e7a1553fd
rsv-k <rsv.kpast@gmail.com>
2020-01-27 15:54:49 +0200

    sea battle code refactored

--- homework/sero/index.js
+++ homework/sero/index.js
@@ -61,10 +61,8 @@ function createRiverShips () {
     SEA BATTLE
 ================ */
 function createSeaBattle () {
-   const shipsCoordinates = [];
    // every call of createSeaBattle generates randomly placed ships
-   // set ships coordinates in passing array;
-   const sea = createSeaMap(shipsCoordinates);
+   const { sea, shipsCoordinates } = createSeaMap();
 
    return function (x) {
       return function (y) {
@@ -95,27 +93,12 @@ function createSeaBattle () {
 }
 
 // map and ships' positioning generator
-function createSeaMap (shipsCoordinates) {
-   const ships = {
-      'four-deck': {
-         amount: 1,
-         size: 4
-      },
-      'three-deck': {
-         amount: 2,
-         size: 3
-      },
-      'two-deck': {
-         amount: 3,
-         size: 2
-      },
-      'one-deck': {
-         amount: 4,
-         size: 1
-      }
-   };
+function createSeaMap () {
+   const ships = getShips();
+   const shipsCoordinates = [];
 
-   let sea = [];
+   const sea = [];
+   // initialize empty map
    for (let i = 0; i < 10; i++) {
       sea.push(new Array(10).fill(0));
    }
@@ -123,46 +106,39 @@ function createSeaMap (shipsCoordinates) {
    let ship;
    let randomX;
    let randomY;
-   let position;
-   let coordinates = [];
+   let rotation;
 
    while (Object.keys(ships).length !== 0) {
       // get ship's name to refer to it in ships' properties
       ship = Object.keys(ships)[0];
-      coordinates = [];
 
+      // position of a ship in x
       randomX = Math.round(Math.random() * (sea.length - ships[ship].size - 1));
+      // position of a ship in y
       randomY = Math.round(Math.random() * (sea.length - ships[ship].size - 1));
 
       if (ships[ship].size > 1) {
-         position = getPosition();
+         rotation = isRotate();
       }
-      if (position === 'rotate') {
-         sea = rotateClockwise(sea);
+      if (rotation) {
+         rotate(sea, true);
       }
 
       // check if ship is not about to be positioned close to another ship
       if (isItClearAround(ships, ship, sea, randomX, randomY)) {
-      // add ship on the map
+         // add ship on the map
          sea[randomY].splice(randomX, ships[ship].size, ...new Array(ships[ship].size).fill(1));
          ships[ship].amount--;
 
          // calculate coordinates of a ship
-         for (let i = randomX; i < randomX + ships[ship].size; i++) {
-            if (position === 'rotate') {
-               coordinates.push(`${9 - i}-${randomY}`);
-            } else {
-               coordinates.push(`${randomY}-${i}`);
-            }
-         }
-
-         // add coordinates of a ship into global array of ships
-         shipsCoordinates.push(coordinates);
+         // and add coordinates of a ship into global array of coordinates
+         shipsCoordinates.push(calculateShipCoordinates(ships, ship, randomX, randomY, rotation));
       }
 
       // rotate it back to initial state
-      if (position === 'rotate') {
-         sea = rotateCounterClockwise(sea);
+      if (rotation) {
+         rotate(sea);
+         rotation = false;
       }
 
       // if the specific ship is no longer need to be positioned delete it from ships obj and move to another ship
@@ -171,7 +147,10 @@ function createSeaMap (shipsCoordinates) {
       }
    }
 
-   return sea;
+   return {
+      sea,
+      shipsCoordinates
+   };
 }
 
 // checker for empty space to put a ship
@@ -189,32 +168,56 @@ function isItClearAround (ships, ship, sea, randomX, randomY) {
    return !surroundings.includes(1);
 }
 
-// 90 degree rotation for positioning ships vertically as well
-function rotateClockwise (matrix) {
-   const arr = [...matrix];
-   const N = arr.length - 1;
-
-   return arr.map((row, i) => row.map((val, j) => arr[N - j][i])
-   );
-}
-function rotateCounterClockwise (matrix) {
-   let arr = [...matrix];
-   // reverse the individual rows
-   arr = arr.map((row) => row.reverse());
-   // swap the symmetric elements
-   for (var i = 0; i < matrix.length; i++) {
-      for (var j = 0; j < i; j++) {
-      // swap the elements
-         [arr[i][j], arr[j][i]] = [arr[j][i], arr[i][j]];
-      }
+// 90 degree rotation clockwise and counterclockwise for positioning ships vertically as well
+function rotate (matrix, isClockwise) {
+   // reverse the rows
+   if (isClockwise) {
+      matrix = matrix.reverse();
+   } else {
+      matrix = matrix.map((row) => row.reverse());
    }
 
-   return arr;
+   // swap the symmetric elements
+   for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < i; j++) {
+         [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+      }
+   }
 };
 
 // a switcher to randomly set ships in vertical or horizontal way
-function getPosition () {
-   return Math.random() - 0.5 > 0 ? 'rotate' : null;
+function isRotate () {
+   return Math.random() - 0.5 > 0;
+}
+
+function getShips () {
+   return {
+      'four-deck': {
+         amount: 1,
+         size: 4
+      },
+      'three-deck': {
+         amount: 2,
+         size: 3
+      },
+      'two-deck': {
+         amount: 3,
+         size: 2
+      },
+      'one-deck': {
+         amount: 4,
+         size: 1
+      }
+   };
+}
+
+function calculateShipCoordinates (ships, ship, randomX, randomY, rotation) {
+   const coordinates = [];
+   for (let i = randomX; i < randomX + ships[ship].size; i++) {
+      coordinates.push(rotation ? `${9 - i}-${randomY}` : `${randomY}-${i}`);
+   }
+
+   return coordinates;
 }
 
 module.exports = {
