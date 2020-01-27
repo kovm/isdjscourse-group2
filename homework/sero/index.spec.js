7ceb6267d2e5376dd24a3bd72ff2cde6a6bb0847
rsv-k <rsv.kpast@gmail.com>
2020-01-27 17:52:23 +0200

    eslint performed

--- homework/sero/index.spec.js
+++ homework/sero/index.spec.js
@@ -1,19 +1,19 @@
 const seaBattle = require('./index').createSeaBattle();
 
 test('Should be one of these: -1, 0, 1', () => {
-  expect([-1, 0, 1]).toContain(seaBattle(0)(0));
-  expect([-1, 0, 1]).toContain(seaBattle(9)(9));
-  expect([-1, 0, 1]).toContain(seaBattle(1)(2));
-  expect([-1, 0, 1]).toContain(seaBattle(1)(0));
-  expect([-1, 0, 1]).toContain(seaBattle(1)(4));
+   expect([-1, 0, 1]).toContain(seaBattle(0)(0));
+   expect([-1, 0, 1]).toContain(seaBattle(9)(9));
+   expect([-1, 0, 1]).toContain(seaBattle(1)(2));
+   expect([-1, 0, 1]).toContain(seaBattle(1)(0));
+   expect([-1, 0, 1]).toContain(seaBattle(1)(4));
 });
 
 test('Should be an exception', () => {
-  expect(() => seaBattle(0)(0)).toThrow();
-  expect(() => seaBattle(10)(0)).toThrow();
-  expect(() => seaBattle()()).toThrow();
-  expect(() => seaBattle(0)(-1)).toThrow();
-  expect(() => seaBattle({})([])).toThrow();
-  expect(() => seaBattle('abc')(0)).toThrow();
-  expect(() => seaBattle(0)('abc')).toThrow();
+   expect(() => seaBattle(0)(0)).toThrow();
+   expect(() => seaBattle(10)(0)).toThrow();
+   expect(() => seaBattle()()).toThrow();
+   expect(() => seaBattle(0)(-1)).toThrow();
+   expect(() => seaBattle({})([])).toThrow();
+   expect(() => seaBattle('abc')(0)).toThrow();
+   expect(() => seaBattle(0)('abc')).toThrow();
 });
